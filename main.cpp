2d6718f9888ccf8195acfd9952fafe4dfd765bd8
rohezal <rashid@milacom.de>
2019-11-09 08:52:05 +0100

    using libpng++

--- main.cpp
+++ main.cpp
@@ -11,22 +11,76 @@ int main()
     //const float half_x = size_x*0.5f;
     //const float half_y = size_y*0.5f;
 
-    png::image< png::gray_pixel> light("light.png");
-    png::image< png::gray_pixel> frequency("frequency.png");
-    png::image< png::gray_pixel> combined("light.png");
+
+    png::image<png::gray_pixel> light("light.png");
+    png::image<png::gray_pixel_16> frequency("frequency.png");
+    png::image<png::gray_pixel> combined("light.png");
 
     const int size_x = light.get_width();
     const int size_y = light.get_height();
+    const int size_bytes = size_x*size_y;
 
-    float* image = new float[size_x*size_y];
+    float* light_array = new float[size_x*size_y];
+    float* frequency_array = new float[size_x*size_y];
+    float* combined_array = new float[size_x*size_y];
+
+    float* image = combined_array;
     float* new_image = new float[size_x*size_y];
-    float* new_image2 = new float[size_x*size_y];
+    //float* new_image2 = combined_array;
+
+    float maximum_frequency = 0;
 
 
+    #pragma omp parallel for
+    for(int y = 0; y < size_y; y++)
+    {
+        for(int x = 0; x < size_x; x++)
+        {
+            const float val_li =  light[y][x];
+            const float val_fre = frequency[y][x];
+            light_array[y*size_x+x] = val_li;
+            frequency_array[y*size_x+x] = val_fre;
+        }
+    }
 
-    exit(0);
+    float max_light = 0;
 
-    const int border = 32;
+    #pragma omp parallel for reduction(max : maximum_frequency)
+    for(int i=0;i<size_bytes; i++)
+    {
+        if(frequency_array[i] > maximum_frequency)
+        {
+            maximum_frequency = frequency_array[i];
+        }
+    }
+
+    std::cout << "maximum_frequency: " << maximum_frequency << std::endl;
+    maximum_frequency = 1/maximum_frequency;
+    std::cout << "maximum_frequency: " << maximum_frequency << std::endl;
+
+    std::cout << "max_light: " << max_light << std::endl;
+
+
+    #pragma omp parallel for
+    for(int y = 0; y < size_y; y++)
+    {
+        for(int x = 0; x < size_x; x++)
+        {
+            const float val_li =  light_array[y*size_x+x];
+            const float val_fre = frequency_array[y*size_x+x];
+            const float val_comb = val_li*val_fre*maximum_frequency; //normalizing the frequency
+            combined_array[y*size_x+x] = val_comb;
+            //combined[y][x] = val_comb;
+
+        }
+    }
+
+    //combined.write("combined.png");
+
+
+    //create a random image
+
+    const int border = 16;
     const int border_half = border/2;
     const int border_squared = border*border;
     const float circle_radius_squared = (border/2)*(border/2);
@@ -44,20 +98,11 @@ int main()
 
     std::cout << "Devices: " << omp_get_num_devices() << std::endl;
 
-    //create a random image
-    #pragma omp parallel for
-    for(int y = 0; y < size_y; y++)
-    {
-        for(int x = 0; x < size_x; x++)
-        {
-            //image[y*size_x+x] = 1/(1+((x-half_x)*(x-half_x)))*1/(1+((y-half_y)*(y-half_y)));
-            image[y*size_x+x] = 1;
-        }
-    }
 
 
     //average on GPU
     double start_time = omp_get_wtime();
+    /*
     #pragma omp target data map(alloc:new_image) map(image)
     {
         #pragma omp target teams distribute parallel for collapse(2)
@@ -71,7 +116,6 @@ int main()
                     {
                         //new_image[y][x] += image[y+border_half][x+border_half];
                         new_image[y*size_x+x] += (a*a+b*b < circle_radius_squared) ? image[(y+a)*size_x+(x+b) ] : 0;
-                        new_image2[(y+a)*size_x+(x+b) ] = (a*a+b*b < circle_radius_squared) ? image[(y+a)*size_x+(x+b) ] : 0;
                     }
                 }
 
@@ -79,11 +123,13 @@ int main()
             }
         }
     }
+    */
 
     std::cout << "Runtime gpu: " << omp_get_wtime()-start_time << std::endl;
 
     //average on CPU
     start_time = omp_get_wtime();
+    //#pragma omp parallel for
     #pragma omp parallel for
     for(int y = border_half; y < size_y-border_half; y++)
     {
@@ -91,9 +137,9 @@ int main()
         {
             for(int a = -border_half; a < border_half; a++)
             {
+                //#pragma omp for simd
                 for(int b = -border_half; b < border_half; b++)
                 {
-                    //new_image[y*size_x+x] += image[(y+a)*size_x+(x+b) ];
                     new_image[y*size_x+x] += (a*a+b*b < circle_radius_squared) ? image[(y+a)*size_x+(x+b) ] : 0;
                 }
             }
